d66c5f6a08ac97dbbd2ad8c11cd4108528e7b7f9
Igor Soares <igor.larcs@gmail.com>
2018-01-17 11:36:02 -0200

    update 5

--- src/projeuler/5/solution.clj
+++ src/projeuler/5/solution.clj
@@ -5,7 +5,7 @@
   (if (every? zero? (map #(mod x %) r)) x (recur (inc x) r)))
 
 ; bellow, recurring straight to 20 took a while, so we start with (inc 15) and move on
-; 232792560 is the equivalent of (find-x 12 (range 1 (inc 19))) that we popped on (inc 20)
+; 232792560 is the equivalent of (find-x 1 (range 1 (inc 19))) that we popped on (inc 20)
 (defn run 
   []
   (find-x 232792560 (range 1 (inc 20))))
